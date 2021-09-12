2683f1a223a754220c646b786aac653197d9db21
Your Name <you@example.com>
2021-09-12 17:11:38 +0000

    html and timer

--- src/main/kotlin/de/diers/clickdelay/ClickResource.kt
+++ src/main/kotlin/de/diers/clickdelay/ClickResource.kt
@@ -5,20 +5,69 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.net.URL;
+import java.util.Properties;
+import java.io.FileInputStream;
+
+import java.util.Timer
+import java.util.Date
+import java.text.SimpleDateFormat
+import kotlin.concurrent.schedule
 
 @RestController
 class ClickResource {
 
     @GetMapping("/click/{id}/{waittime}")
-    fun click(@PathVariable id:String, @PathVariable waittime:Int): String {
+    fun click(@PathVariable id:String, @PathVariable waittime:Long): String {
 
-        //on        
-        URL("https://google.com").readText()
+        val sdf = SimpleDateFormat("dd/MM/yyyy HH24:mm:ss")
 
-        var envVar: String = System.getenv("tasmota.one") ?: "n/a"
+        //read switch url    
+        var envVar = getProp<String>("tasmota."+id+".switch")
 
-        return "clicked: "+id+" - waiting for "+waittime+" sec "+envVar
+        //switch
+        var status = URL(envVar).readText()        
+        println(sdf.format(Date())+" clicked "+id+": "+status)
+
+        //create timer to stop it - we can do this for every execution...
+        Timer("StopTimer", false).schedule(1000*waittime) { 
+            if (status(id).contains("ON")) {
+                println(sdf.format(Date())+" timer "+id+": "+URL(envVar).readText())
+            } else {
+                println(sdf.format(Date())+" timer "+id+": is off")
+            }
+        }
+
+        return status(id)
+    }
+
+    @GetMapping("/status/{id}")
+    fun status(@PathVariable id:String): String {
         
+        //read status url    
+        var envVar = getProp<String>("tasmota."+id+".status")
+
+        //read status      
+        var status = URL(envVar).readText()
+
+        return status        
+    }
+
+    @GetMapping("/color/{id}")
+    fun color(@PathVariable id:String): String {
+        
+        if (status(id).contains("ON")) {
+            return "#DAF7A6"
+        } else {
+            return "#FF5733"
+        }               
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    fun <T> getProp(key: String): T {
+        val props  = javaClass.classLoader.getResourceAsStream("application.properties").use {
+            Properties().apply { load(it) }
+        }
+        return (props.getProperty(key) as T) ?: throw RuntimeException("could not find property $key")
     }
 
 }
